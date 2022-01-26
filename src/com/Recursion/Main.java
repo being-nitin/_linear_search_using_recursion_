3ea88980e12233ab05a906dacccc32b8de182af1
Nitin <nsinghboss02@gmail.com>
2022-01-26 12:46:17 +0530

    if target element is more than one that is multiple occurance.

--- src/com/Recursion/Main.java
+++ src/com/Recursion/Main.java
@@ -8,9 +8,11 @@ public class Main {
 	1 . check whether the target element is present or not.
 	2 . return the index of the target element.
 	 */
-      int[] arr = {5,7,3,9,1};
+      int[] arr = {5,7,3,9,7,1};
         System.out.println(find(arr,7,0));
         System.out.println(findIndex(arr,7,0));
+        findAllIndex(arr,7,0);
+        System.out.println(list);
 
     }
     // first part.
@@ -20,6 +22,7 @@ public class Main {
         }
         return arr[index]==target || find(arr,target,index+1);
     }
+    // second part.
     public static int findIndex(int[] arr,int target,int index){
         if(index==arr.length){
             return -1;
@@ -28,5 +31,16 @@ public class Main {
             return index;
         }
         return findIndex(arr,target,index+1);
+    }
+    // if target element is more than one, that is there is multiple occurance of the target element;
+    static ArrayList <Integer> list = new ArrayList<>();
+    public static void findAllIndex(int[] nums,int target,int index){
+        if(index==nums.length){
+            return;
+        }
+        if(nums[index]==target){
+            list.add(index);
+        }
+        findAllIndex(nums,target,index+1);
     }
 }
